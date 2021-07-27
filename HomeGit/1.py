d3f61c7a9e3631efaddc3192201caa27e86a087a
Inna <lesuninv@gmail.com>
2021-07-27 19:15:21 +0300

    1 added

---
+++ HomeGit/1.py
@@ -0,0 +1,12 @@
+f = open('1.txt', 'r')
+
+# for i in f:
+#     i = int(i[len(i) - 2])
+# #    print(i)
+# = open('class8.txt')
+
+for i in f:
+    print(i)
+    print(len(i))
+
+    g = int(i[len(i)-2])
